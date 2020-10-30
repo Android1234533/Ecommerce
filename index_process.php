52cf05a3a6a0b5aae363f65d6feb36ef525e1529
Sonalika <73528056+Sonalika2341235rrr3r3r@users.noreply.github.com>
2020-10-30 22:44:02 +0530

    Create index_process.php

---
+++ index_process.php
@@ -0,0 +1,22 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+$con = mysqli_connect('173.212.236.58:3306 (MySQL)', 'wp_nr6us', 'Jaimatadi@123','wp_nr6us');
+
+// get the post records
+$name = $_POST['name'];
+$lastName = $_POST['lastName'];
+$emailId = $_POST['emailId'];
+$contactNum = $_POST['contactNum'];
+
+// database insert SQL code
+$sql = "INSERT INTO `UserDetails` (`id`, `Name`, `LastName`, `Email`, `Contactinfo`) VALUES ('0', '$name', '$lastName', '$emailId', '$contactNum')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+?>
+ 
