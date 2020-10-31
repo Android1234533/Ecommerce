ff8475621e363debb14eb31f4a7d301266e8be66
Sonalika <73528056+Sonalika2341235rrr3r3r@users.noreply.github.com>
2020-10-31 22:14:14 +0530

    Update index_process.php

--- index_process.php
+++ index_process.php
@@ -2,22 +2,25 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 $con = mysqli_connect('173.212.236.58:3306 (MySQL)', 'wp_nr6us', 'Jaimatadi@123','wp_nr6us');
+$check="SELECT * FROM UserDetails WHERE Email  = '$_POST[emailId]'";
+$rs = mysqli_query($con,$check);
+$data = mysqli_fetch_array($rs, MYSQLI_NUM);
+if($data[0] > 1) {
+    echo "Entered Data Is Already Exist !<br/>";
+} 
 
-// get the post records
-$name = $_POST['name'];
-$lastName = $_POST['lastName'];
-$emailId = $_POST['emailId'];
-$contactNum = $_POST['contactNum'];
-
-// database insert SQL code
-$sql = "INSERT INTO `UserDetails` (`id`, `Name`, `LastName`, `Email`, `Contactinfo`) VALUES ('0', '$name', '$lastName', '$emailId', '$contactNum')";
-
-// insert in database 
-$rs = mysqli_query($con, $sql);
-if($rs)
+else
 {
-	echo "Contact Records Inserted";
+    $newUser="INSERT INTO UserDetails(Name,LastName,Email,Contactinfo) values('$_POST[name]','$_POST[lastName]','$_POST[emailId]','$_POST[contactNum]')";
+    if (mysqli_query($con,$newUser))
+    {
+        echo "Congratulations! Your Data has been Submitted Successfully.<br/>";
+    }
+    else
+    {
+        echo "Error adding user in database<br/>";
+    }
 }
 ?>
- 
+
  
