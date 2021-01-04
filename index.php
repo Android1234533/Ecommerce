db85f78876b8c35d9e641716c151b1290306375d
Android1234533 <37035715+Android1234533@users.noreply.github.com>
2021-01-04 23:02:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,52 +8,71 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
   <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+  <link rel="stylesheet" href="stylesheet.css">
   <style>
+	  input[type="submit"] {
+    outline: none !important;
+    box-shadow: none !important;
+}
+	  	  input[type="text"] {
+    outline: none !important;
+    box-shadow: none !important;
+}
+	  	  	  input[type="number"] {
+    outline: none !important;
+    box-shadow: none !important;
+}
+	  	  	  input[type="email"] {
+    outline: none !important;
+    box-shadow: none !important;
+}
+
 	  html{
      scroll-behavior:smooth;
-     }
-  </style>
-	  
-  </head>	
+     } 
+	  </style>	  
+ </head>	
 <body>  
-<nav class="navbar navbar-expand-sm " style="background-color:#e91e63; border: 3px solid #e91e63;">
-  <div class="container">
-  <a class="navbar-brand h3">Tech Inject</a>
-  <button class="navbar-toggler" data-toggle="collapse" data-target="#Header">
+<nav class="navbar navbar-expand-sm">
+  <div class="container">	  
+	  <a class="navbar-brand h3"> <h1 class="Logo">PS</h1><u id="mylogocolor">Prosolutions</u></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Header">
     <span class="fa fa-bars" style="color:#fff;"></span>
   </button>
-  <div class="collapse navbar-collapse" id="Header">
-       <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a href="#Home active" class="nav-link">Home</a>
+  <div class="collapse navbar-collapse" id="Header"> 
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">			
+			<a href="#Home active" class="nav-link dos bis"><h2>Home</h2></a>
         </li>
         <li class="nav-item">
-          <a href="#aboutUs" class="nav-link" >About Us</a>   
+			<a href="#aboutUs" class="nav-link dos bis"><h2>About Us</h2></a>   
         </li>  
         <li class="nav-item active">
-          <a href="#products" class="nav-link">Products</a>
+			<a href="#products" class="nav-link dos bis"><h2>Products</h2></a>
         </li>
-        <li class="nav-item" >
-          <a href="#contactUs" class="nav-link">Contact Us</a>
+        <li class="nav-item">
+			<a href="#contactUs" class="nav-link dos bis"><h2>Contact Us</h2></a>
         </li>
       </ul>
       <form class="form-inline">
-         <button class="btn-login">Login</button>
+		
+		  <button class="btn-login"><a href="Login.php" class="btn-login">Login >></a></button>
       </form>
-    </div>  
+    </div>   
   </div>
-  </nav>
+  </nav> 
   <section class="Home" id="Home">
        <img src="img/Online-Shopping-Banner.png" class="img-fluid" style="width:100%"/>
-  </section>
+      </section>
+
   <section class="aboutUs" id="aboutUs">
-    <h2 class="headings" style="text-align: center;"> About Us!</h2>    
+    <h2 class="headings" style="text-align: center;"> About Us!</h2>
+    
     <div class="container">
       <div class="row">
-        <div class="col-lg-6" id="left-About">          
+        <div class="col-lg-6" id="left-About">
+          
           <h3 class="hdAbt">Who we are</h3>
           <p class="pAbt"> We are guided by four principles: customer obsession rather than competitor focus, passion for invention, commitment to operational excellence, and long-term thinking. </p>
           <h3 class="hdAbt">What we are Providing</h3>
@@ -61,7 +80,7 @@
           <h3 class="hdAbt">Why Buy Our Products </h3>
           <p class="pAbt"> We are Providing International Products. Here you are saving your valuable time rather than go to Accessories Products shop.</p>
        </div>
-     <div class="col-lg-6 " id="right-About">
+      <div class="col-lg-6 " id="right-About">
      <figure class="figure">
       <img src="https://images-na.ssl-images-amazon.com/images/I/71sP8fzZACL._UL1249_.jpg" class="figure-img img-fluid rounded mx-auto d-block" style="box-shadow: 0 0 24px 0 rgba(0, 0, 0, 0.12);"/>          
           <figcaption class="figure-caption">
@@ -74,33 +93,35 @@
               <span class="fa fa-star"></span>
               <span class="fa fa-star"></span>
               <span class="ratings-amount">(6400)</span>       
-            </div>
+              </div>
             <div class="PriceTag">
               <span class="sizefa"><i class="fa fa-rupee"></i></span>
               <span class="sizefa">499/-</span>
-              <div class="spaceGap"></div>
+                <div class="spaceGap"></div>
               <a href="#" class="addbtn"><span>ADD TO CART</span></a>
-            </div>
+              </div>
           </figcaption>
      </figure>
      </div>
     </div>
     </div> 
   </section>
+
   <section class="products" id="products">
     <h2 class="headings"> Our Products </h2>
     <div class="row">
+     
     <div class="col-lg-6 rounded mx-auto d-block" style="border: 1px solid #ccc;">
       <figure class="figure">
         <div class="carousel slide" data-ride="carousel" id="mycrousel1">
-	  <ol class="carousel-indicators">
-	       <li data-target="#mycrousel1" data-slide-to="0" class="active"></li>
-	       <li data-target="#mycrousel1" data-slide-to="1"></li>
-	       <li data-target="#mycrousel1" data-slide-to="2"></li>	
-	   </ol>
-            <div class="carousel-inner">
+			<ol class="carousel-indicators">
+				<li data-target="#mycrousel1" data-slide-to="0" class="active"></li>
+	    		<li data-target="#mycrousel1" data-slide-to="1"></li>
+				<li data-target="#mycrousel1" data-slide-to="2"></li>	
+			</ol>
+          <div class="carousel-inner">
             <div class="carousel-item active">
-             <img src="img/product1(i).jpg" style="width:300px; height:300px"class="figure-img rounded mx-auto d-block"/>
+              <img src="img/product1(i).jpg" style="width:300px; height:300px"class="figure-img rounded mx-auto d-block"/>
             </div>
             <div class="carousel-item">
              <img src="img/product1(ii).jpg" style="width:300px; height:300px" class="figure-img rounded mx-auto d-block"/>
@@ -118,7 +139,8 @@
         </div>
         <figcaption class="figure-caption">
           <h1 class="producth1">Deniel Wellington</h1>
-	    <p class="pAbt">Deniel Wellington Women's Pretite Melrose Strap Analogue Watch.</p>           
+			 <p class="pAbt">Deniel Wellington Women's Pretite Melrose Strap Analogue Watch.</p>
+           
           <div class="item-ratings">
             <span class="fa fa-star"></span>
             <span class="fa fa-star"></span>
@@ -126,12 +148,12 @@
             <span class="fa fa-star"></span>
             <span class="fa fa-star"></span>
             <span class="ratings-amount">(6400)</span>       
-          </div>
+            </div>
           <div class="PriceTag">
             <span class="sizefa"><i class="fa fa-rupee"></i></span>
             <span class="sizefa">499/-</span>
            <div class="spaceGap"></div>
-	        <a href="#" class="addbtn"><span>ADD TO CART</span></a>
+			  <a href="#" class="addbtn"><span>ADD TO CART</span></a>
             </div>   
         </figcaption>
       </figure>  
@@ -140,11 +162,11 @@
   <figure class="figure">
     <div class="carousel slide" data-ride="carousel" id="mycrousel2">
      <ol class="carousel-indicators">
-	<li data-target="#mycarousel2" data-slide-to="0" class="active"></li>
-	<li data-target="#mycarousel2" data-slide-to="1"></li>
-        <li data-target="#mycarousel2" data-slide-to="2"></li>	
-     </ol>
-    <div class="carousel-inner">
+				<li data-target="#mycarousel2" data-slide-to="0" class="active"></li>
+	    		<li data-target="#mycarousel2" data-slide-to="1"></li>
+				<li data-target="#mycarousel2" data-slide-to="2"></li>	
+			</ol>
+		<div class="carousel-inner">
         <div class="carousel-item active">
           <img src="img/product2(i).jpg" style="width:300px; height:300px" class="figure-img rounded mx-auto d-block"/>
         </div>
@@ -154,17 +176,18 @@
         <div class="carousel-item">
          <img src="img/product2(iii).jpg" style="width:300px; height:300px" class="figure-img rounded mx-auto d-block"/>
         </div>
-    </div>
+      </div>
       <a class="carousel-control-prev" href="#mycrousel2" data-slide="prev">
        <span class="carousel-control-prev-icon" ></span>
-      </a>
-      <a class="carousel-control-next" href="#mycrousel2" data-slide="next">
+     </a>
+     <a class="carousel-control-next" href="#mycrousel2" data-slide="next">
        <span class="carousel-control-next-icon" ></span>
-      </a>
-  </div>
-     <figcaption class="figure-caption">
+     </a>
+    </div>
+    <figcaption class="figure-caption">
       <h1  class="producth1">Espoir Analog Black</h1>
-	<p class="pAbt">Espoir Analog Black Dial Day and Date Boy's and Men's Watch Hero bahu0507</p>           
+		 <p class="pAbt">Espoir Analog Black Dial Day and Date Boy's and Men's Watch Hero bahu0507</p>
+           
       <div class="item-ratings">
         <span class="fa fa-star"></span>
         <span class="fa fa-star"></span>
@@ -172,79 +195,79 @@
         <span class="fa fa-star"></span>
         <span class="fa fa-star"></span>
         <span class="ratings-amount">(6400)</span>       
-      </div>
+        </div>
       <div class="PriceTag">
         <span class="sizefa"><i class="fa fa-rupee"></i></span>
         <span class="sizefa">499/-</span>
           <div class="spaceGap"></div>
         <a href="#" class="addbtn"><span>ADD TO CART</span></a>
-      </div>   
+        </div>   
     </figcaption>
-    </figure> 
-    </div>
-    </div>
-</section>
-<section class="contactUs" id="contactUs">
-   <h2 class="headings"> Contact Us! </h2>
-   <div class="row"> 
-    <div class="col-lg-6" class="contact-form">			
-      <div class="row" id="row-cont2">
-        <div class="col-lg-12">
-          <div class="address">
-	    <i class='fas fa-map-marker-alt' style="font-size:24px"></i>				  
-	    <h6 class="h6">Location:</h6>
-	    <p class="p">Jalandhar, Jalandhar, Punjab 144001</p>
-         </div>
-        </div> 
-        <div class="col-lg-12">
-	  <div class="email"> 
-	    <i class='fas fa-briefcase' style='font-size:24px'></i>
-	    <h6 class="h6">Email Id:</h6>
-            <p class="p">techinjectindia@gmail.com</p>
-	  </div>
-	</div> 
-        <div class="col-lg-12">
-         <div class="contact">
-	    <i class='fas fa-phone' style='font-size:24px'></i>
-	    <h6 class="h6">Contact Number:</h6>
-	    <p class="p">088 262 16 673</p>				 
-	 </div>
-       </div>			  
-     </div>
- </div>	    
-<div class="col-lg-6"> 
-  <div class="contact-form">
-    <h1 class="h1-cus"> Drop Your Details here!</h1>
+  </figure> 
+</div>
+  </div>
+  </section>
+  <section class="contactUs" id="contactUs">
+    <h2 class="headings"> Contact Us! </h2>
+    <div class="row"> 
+ 		<div class="col-lg-6" class="contact-form">			
+			 <div class="row" id="row-cont2">
+			  <div class="col-lg-12">
+				  <div class="address">
+				  <i class='fas fa-map-marker-alt' style="font-size:24px"></i>				  
+				  <h6 class="h6">Location:</h6>
+				  <p class="p">Jalandhar, Jalandhar, Punjab 144001</p>
+				 </div>
+		      </div> 
+				 <div class="col-lg-12">
+				  <div class="email"> 
+					<i class='fas fa-briefcase' style='font-size:24px'></i>
+					  <h6 class="h6">Email Id:</h6>
+					 <p class="p">sharmasonalika49@gmail.com</p>
+				 </div>
+				 </div> 
+					 <div class="col-lg-12">
+				  <div class="contact">
+					  <i class='fas fa-phone' style='font-size:24px'></i>
+					   <h6 class="h6">Contact Number:</h6>
+					 <p class="p">+91 76962-11486</p>				 
+					</div>
+				 </div>			  
+			</div>
+		</div>
+		  <div class="col-lg-6"> 
+    <div class="contact-form">
+      <h1 class="h1-cus"> Drop Your Details here!</h1>
      <form class="myForm" id="myForm" action="index_process.php" method="post" name="myForm" enctype="multipart/form-data">
        <div class="row">
          <div class="col-lg-6">
-             <label class="label">Name *</label>
-	     <input type="text" class="name" id="name" name="name" required/>
+			 <label class="label">Name *</label>
+			 <input type="text" class="name" id="name" name="name" required/>
           </div>
-	<div class="col-lg-6"> 
-	     <label class="label">Last Name *</label>
-	     <input type="text" class="lastName" id="lastName" name="lastName" required/>
-        </div>
+		    <div class="col-lg-6"> 
+			 <label class="label">Last Name *</label>
+			 <input type="text" class="lastName" id="lastName" name="lastName" required/>
+         </div>
        </div>
       <div class="row">
          <div class="col-lg-6">
-	    <label class="label">Email Id *</label>
-	    <input type="email" class="emailId" id="emailId" name="emailId" required />
+			 <label class="label">Email Id *</label>
+			 <input type="email" class="emailId" id="emailId" name="emailId" required />
          </div>
-         <div class="col-lg-6">
-	   <label class="label">Contact Number *</label>
-	   <input type="number" class="contactNum" id="contactNum" name="contactNum" required />			
+		    <div class="col-lg-6">
+			 <label class="label">Contact Number *</label>
+			 <input type="number" class="contactNum" id="contactNum" name="contactNum" required />			
          </div>
-      </div>		  
-      <div class="row">
+       </div>		  
+		 <div class="row">
         <div class="col-lg-12">
-           <input type="submit" class="submit" id="submit" name="submit"  value="Submit here" >
+          <input type="submit" class="submit" id="submit" name="submit"  value="Submit here" >
         </div>
       </div> 
      </form>
    </div>
 </div>		
 </div>
-</section> 
-</body>
+</section>
+</body>	
 </html>
